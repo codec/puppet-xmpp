180c6df4fc855dcc341f3af45989d486fd0cde40
codec <codec@fnord.cx>
2012-06-20 18:33:01 +0200

    sigh

--- lib/puppet/reports/xmpp.rb
+++ lib/puppet/reports/xmpp.rb
@@ -26,7 +26,7 @@ Puppet::Reports.register_report(:xmpp) do
   def process
     if self.environment == nil
       Puppet.info "environment for #{self.host} was nil, is now #{XMPP_ENV}"
-      self.environment = config[XMPP_ENV]
+      self.environment = XMPP_ENV
     end
 
     if self.status == 'failed' and (XMPP_ENV.include?(self.environment?) or XMPP_ENV == 'ALL')
